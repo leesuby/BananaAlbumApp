f03eb61615611bfe116128979377013666539c4f
dat-badboy <64117485+dat-badboy@users.noreply.github.com>
2022-04-04 17:35:47 +0700

    sign in with google ver_01

--- app/src/main/java/com/example/bananaalbum/views/Login.java
+++ app/src/main/java/com/example/bananaalbum/views/Login.java
@@ -3,6 +3,7 @@ package com.example.bananaalbum.views;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,8 +14,18 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bananaalbum.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class Login extends AppCompatActivity {
+    GoogleSignInClient mGoogleSignInClient;
+    static final int RC_SIGN_IN = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -37,7 +48,7 @@ public class Login extends AppCompatActivity {
         Button SignIn = (Button) findViewById(R.id.ButtonSignIn);
         Button Google = (Button) findViewById(R.id.ButtonGoogle);
         Button Facebook = (Button) findViewById(R.id.ButtonFacebook);
-        TextView ForgorPassword = (TextView) findViewById(R.id.ButtonForgotPassword);
+        TextView ForgotPassword = (TextView) findViewById(R.id.ButtonForgotPassword);
         TextView SignUp = (TextView) findViewById(R.id.ButtonSignUp);
         //Action Sign in
         SignIn.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +65,23 @@ public class Login extends AppCompatActivity {
             }
 
         });
+        //Action Sign in with Google
+        SignInButton signInButton;
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.ButtonGoogle:
+                        signIn();
+                        break;
+                }
+            }
+        });
         //Action Sign Up
         SignUp.setPaintFlags(SignUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         SignUp.setOnClickListener(new View.OnClickListener() {
@@ -63,5 +91,33 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(this, "Sign-in Successfully",Toast.LENGTH_SHORT).show();
+            ToMainScreen(account);
+        } catch (ApiException e) {
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+    private void ToMainScreen(GoogleSignInAccount account){
+        Intent intent = new Intent(this, MainScreen.class);
+        startActivity(intent);
     }
 }
