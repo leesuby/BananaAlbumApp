ca5d3256b17ab5f9cb8f07165f8e6e76054d10e8
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-04-09 04:15:58 +0700

    update user setting view and create detail image view

---
+++ app/src/main/java/com/example/bananaalbum/views/DetailImage.java
@@ -0,0 +1,17 @@
+package com.example.bananaalbum.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.bananaalbum.R;
+
+public class DetailImage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_image);
+    }
+}
---
+++ app/src/main/java/com/example/bananaalbum/views/User_setting.java
@@ -0,0 +1,17 @@
+package com.example.bananaalbum.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.bananaalbum.R;
+
+public class User_setting extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_setting_layout);
+    }
+}
