c5e98622e17f38c2ee56c52c166004dd61a958de
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-04-12 17:06:28 +0700

    connect android app to mysql database

--- app/src/main/java/com/example/bananaalbum/views/User_setting.java
+++ app/src/main/java/com/example/bananaalbum/views/User_setting.java
@@ -1,11 +1,22 @@
 package com.example.bananaalbum.views;
 
+import static com.example.bananaalbum.utils.DatabaseConnector.createConnection;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bananaalbum.R;
+import com.example.bananaalbum.model.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class User_setting extends Activity {
 
@@ -13,5 +24,40 @@ public class User_setting extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_setting_layout);
+
+        TextView name,email,bdate,addr;
+        name= findViewById(R.id.user_name);
+        email=findViewById(R.id.user_email);
+        bdate=findViewById(R.id.user_bdate);
+        addr=findViewById(R.id.user_addr);
+        Account user = null;
+        String sql;
+        ResultSet rs;
+
+        Connection conn = createConnection();
+        PreparedStatement psm = null;
+
+        try {
+            sql = "SELECT * FROM users where username = ?";
+            psm = conn.prepareStatement(sql);
+            psm.setString(1, "thienlong");
+            rs = psm.executeQuery();
+
+            while (rs.next()) {
+                user = new Account(rs.getString("name"), rs.getString("email"), rs.getDate("dob").toString()
+                        , rs.getNString("address"));
+            }
+
+            conn.close();
+            psm.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        name.setText(user._name);
+        email.setText(user._email);
+        bdate.setText(user._bdate);
+        addr.setText(user._address);
+
+
     }
 }
