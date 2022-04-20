421255322687fc35c7d4022cf15be2d596ff6f8b
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-04-20 23:04:45 +0700

    update detail view user settings

---
+++ app/src/main/java/com/example/bananaalbum/views/ChangePassword.java
@@ -0,0 +1,18 @@
+package com.example.bananaalbum.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import com.example.bananaalbum.R;
+
+public class ChangePassword extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
---
+++ app/src/main/java/com/example/bananaalbum/views/UpdateProfile.java
@@ -0,0 +1,16 @@
+package com.example.bananaalbum.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bananaalbum.R;
+
+public class UpdateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+    }
+}
