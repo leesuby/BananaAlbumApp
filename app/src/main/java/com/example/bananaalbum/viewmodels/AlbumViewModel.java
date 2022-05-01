0c5fe337f790fe5788361e2d2316aee53805dd8b
dat-badboy <64117485+dat-badboy@users.noreply.github.com>
2022-05-02 04:21:40 +0700

    load album from realtime database

--- app/src/main/java/com/example/bananaalbum/viewmodels/AlbumViewModel.java
+++ app/src/main/java/com/example/bananaalbum/viewmodels/AlbumViewModel.java
@@ -1,13 +1,26 @@
 package com.example.bananaalbum.viewmodels;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 
 import com.example.bananaalbum.model.Album;
+import com.example.bananaalbum.views.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class AlbumViewModel extends ViewModel {
@@ -32,7 +45,29 @@ public class AlbumViewModel extends ViewModel {
     public MutableLiveData<List<Album>> getListAlbumLiveData() {
         return listAlbumLiveData;
     }
+    public void loadData(String userID){
 
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("data").child(userID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listAlbum = new ArrayList<>();
+                Iterable<DataSnapshot> snapshotIterator = snapshot.getChildren();
+                Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
+                while (iterator.hasNext()) {
+                    DataSnapshot next = (DataSnapshot) iterator.next();
+                    listAlbum.add(new Album(next.child("name").getValue().toString()));
+                }
+                listAlbumLiveData.setValue(listAlbum);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("TAG", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+    }
     public void setListAlbumLiveData(ArrayList<Album> list) {
         listAlbumLiveData.setValue(list);
     }
